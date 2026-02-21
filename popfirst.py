001da44ed229f62668f41f037a559c28420670eb
AHEM SAXENA <99779094+ahemsaxena07@users.noreply.github.com>
2026-02-22 02:48:40 +0530

    last progress
    adding a concept of pop first in linked list

---
+++ popfirst.py
@@ -0,0 +1,40 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkeList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        self.tail.next = new_node
+        self.tail = new_node
+        self.length += 1
+
+    def popfirst(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+        return temp
+    
+my_list = LinkeList(1)
+my_list.append(2)
+my_list.print_list()
+my_list.popfirst()
+my_list.print_list()
