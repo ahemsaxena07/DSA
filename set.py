a6df517437c0643452d3d491611a830146e7c4ff
AHEM SAXENA <99779094+ahemsaxena07@users.noreply.github.com>
2026-03-10 20:22:53 +0530

    last progress
    adding concept of set value in a linked list

---
+++ set.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        self.tail.next = new_node
+        self.tail = new_node
+        self.length += 1
+
+    def get(self, index):
+        if index < 0 or index >=  self.length:
+            return None
+        temp = self.head 
+        for _ in range(index):
+            temp = temp.next
+        return temp
+    
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+
+my_list = LinkedList(0)
+my_list.append(1)
+my_list.append(4)
+my_list.append(7)
+my_list.set_value(1,3)
+my_list.print_list()
